4906c38ef6d2530ea1a1146b96c0e26a22c33dc4
Leonardo Monteiro Mastra Fontoura <leonardommfontoura@gmail.com>
2020-08-11 22:41:21 -0300

    logica de refresh da pagina adicionado

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 let colors = generateRandomColors(6); 
-
-
 let squares = document.querySelectorAll(".square")
 let pickedColor = pickColor();
 let colorDisplay = document.getElementById("colorDisplay");
 let messageDisplay = document.querySelector('#message');
 let h1 = document.querySelector('h1');
+let resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+	//generate all new colors
+	colors = generateRandomColors(6);
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//change colorDisplay to match picked Color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares
+	for(let i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+	}
+	h1.style.background = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
 for(let i = 0; i < squares.length; i++){
 	// add initial colors to squares
 	squares[i].style.background = colors[i];
-
 	//add click listeners to squares
 	squares[i].addEventListener("click", function() {
 		//grab color of clicked squares
@@ -20,6 +32,7 @@ for(let i = 0; i < squares.length; i++){
 		//compare color to pickedColor
 		if(clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct";
+            resetButton.textContent = "Play again?";
             changeColors(clickedColor);
             h1.style.background = clickedColor;
 		} else {
@@ -29,14 +42,12 @@ for(let i = 0; i < squares.length; i++){
 	});
 }
 
-
 function changeColors(color){
     for(let i = 0; i < squares.length; i++) {
         squares[i].style.background = color;
     }
 }
  
-
 function pickColor() {
     let random = Math.floor(Math.random() * colors.length); 
     return colors[random];
@@ -47,11 +58,8 @@ function generateRandomColors(num) {
     for(let i = 0; i < num; i++) {
         arr.push(randomColor());
     }
-
-
     return arr;
 }
-
 
 function randomColor() {
     let r = Math.floor(Math.random() * 256);
