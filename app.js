1a882319b3c6872edb8ea95198c6dfe299c78063
Leonardo Monteiro Mastra Fontoura <leonardommfontoura@gmail.com>
2020-08-11 21:00:56 -0300

    logica das cores adicionadas junto com mudanca nos quadrados quando clicados

--- app.js
+++ app.js
@@ -4,21 +4,33 @@
      'rgb(0, 255, 0)',
      'rgb(0, 255, 255)',
      'rgb(0, 0, 255)',
-     'rgb(255, 0, 0)'    
+     'rgb(255, 0, 255)'    
 ];
 
 
 let squares = document.querySelectorAll(".square")
 let pickedColor = colors[3];
-let colorDisplay = document.getElementById('colorDisplay');
+let colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
-for(let i = 0; i< squares.length; i++) {
-    squares[i].style.backgroundColor = colors[i];
+for(let i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
+
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		let clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			alert("Correct!");
+		} else {
+			this.style.background = "#232323";
+		}
+	});
 }
-
-
+ 
 
 
 
